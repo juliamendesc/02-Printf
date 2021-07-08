7ff9e48b06f46beabb8ea78997d040d24b75593c
Julia <julia@bocals-iMac.local>
2021-07-08 13:48:30 +0100

    begin 3

--- ft_printf.c
+++ ft_printf.c
@@ -1,79 +1,24 @@
 
 #include "ft_printf.h"
 
-t_flags ft_init_tab(void)
+t_flags tab
 {
-	t_flags	tab;
-
-	tab.dot = -1;
-	tab.minus = 0;
-	tab.star = 0;
-	tab.type = 0;
-	tab.width = 0;
-	tab.zero = 0;
+	dot = -1;
+	minus = 0;
+	hash = 0;
+	width = 0;
+	zero = 0;
+	plus = 0;
+	total_length = 0;
 	return(tab);
 }
 
-int	ft_check_flags(char c)
+void	ft_parse_flags(const char *format)
 {
-	if (!ft_which_type(c) && !ft_isdigit(c) && !ft_is_flag(c))
-		return (1);
-	return (0);
-}
-
-int	ft_parse_flags(const char *format, int i, t_flags *tab, va_list args)
-{
-	while (format[i])
-	{
-		if (ft_check_flags(format[i]))
-			break ;
-		if (format[i] == '0'&& !(tab->minus) && !(tab->width))
-			tab->zero = 1;
-		if (format[i] == '.')
-			i = ft_flag_dot(format, i, tab, args);
-		if (format[i] == '-')
-			*tab = ft_flag_minus(*tab);
-		if (format[i] == '*')
-			*tab = ft_flag_width(args, *tab);
-		if (ft_isdigit(format[i]))
-		{
-			if (tab->star == 1)
-				tab->width = 0;
-			tab->width = (tab->width * 10) + (format[i] - '0');
-		}
-		if (ft_which_type(format[i++]))
-		{
-			tab->type = format[--i]; //pq?
-			break ;
-		}
-	}
-	return (i);
-}
-
-int	ft_incoming(const char *format, va_list args)
-{
-	int		i;
-	int		count;
 	t_flags	tab;
 
-	i = 0;
-	count =0;
-	while (format[i])
-	{
-		tab = ft_init_tab();
-		if (format[i] == '%' && format[i+1])
-		{
-			i = ft_parse_flags(format, ++i, &tab, args);
-			if (ft_which_type(format[i]))
-				count += ft_convert((char)tab.type, tab, args);
-			else
-				return (count);//verificar se nao é count += ft_putchar(format[i])
-		}
-		else if (format[i] != '%')
-			count += ft_putchar_returnint(format[i]);
-		i++;
-	}
-	return (count);
+	ft_init_tab();
+	printf("struct %d", tab.dot);
 }
 
 int	ft_printf(const char *format, ...)
@@ -83,7 +28,8 @@ int	ft_printf(const char *format, ...)
 
 	count = 0;
 	va_start(args, format);
-	count += ft_incoming(format, args);
+	ft_parse_flags(format);
+	//check type
 	va_end(args);
 	return (count);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -4,39 +4,20 @@
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include "libft/libft.h"
 
 typedef	struct s_flags
 {
-	int		type;
-	int		width;
-	int		prec;
-	int		minus;
-	int		zero;
 	int		dot;
-	int		dash;
-	int		star;
+	int		minus;
+	int		hash;
+	int		width;
+	int		zero;
+	int		plus;
 	int		total_length;
 }			t_flags;
 
 int	ft_printf(const char *format, ...);
-int	ft_incoming(const char *format, va_list args);
-int	ft_parse_flags(const char *format, int i, t_flags *tab, va_list args);
-int	ft_check_flags(char c);
-t_flags ft_init_tab(void);
-int	ft_is_flag(char c);
-int	ft_which_type(char c);
-int	ft_flag_dot(const char *format, int i, t_flags *tab, va_list args);
-t_flags	ft_flag_minus(t_flags tab);
-t_flags	ft_flag_width(va_list args, t_flags tab);
-int	ft_convert(char type, t_flags tab, va_list args);
-int	ft_width_treat(int width, int slots, int includes_zero);
-int	ft_putchat_returnint(char c);
-int	ft_convert_char(int c, t_flags tab);
-int	ft_putstr_len(char *str, size_t len);
-int	ft_convert_string(char *str, t_flags tab);
-int	ft_putint_len(char *str, t_flags tab);
-int	ft_length(char *str, t_flags tab, int i);
-int	ft_handle_int(char *str, t_flags *tab, int len);
 
 #endif
